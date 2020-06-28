9d23c2a857ccfcbe80a1e063c3eba1f9d7813da2
Ernest Gilyazov <gilyazov@reg.ru>
2020-06-28 13:45:32 +0300

    Class for working with DOM

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,22 +1,20 @@
+import { $ } from '@core/dom';
+
 export class Excel {
     constructor( selector, options ) {
-        this.$el = document.querySelector( selector );
+        this.$el = $( selector );
         this.components = options.components || [];
     }
 
     getRoot() {
-        const $root = document.createElement('div');
-        
-        $root.classList.add('excel');
+        const $root = $.create( 'div', 'excel' );
 
         this.components.forEach( Component => {
-            const $el = document.createElement('div');
-
-            $el.classList.add( Component.className );
+            const $el = $.create( 'div', Component.className );
 
             const component = new Component( $el );
 
-            $el.innerHTML = component.toHTML();
+            $el.html( component.toHTML() );
 
             $root.append( $el );
         } );
