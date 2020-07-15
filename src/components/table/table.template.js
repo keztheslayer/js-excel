2b86020fd8d7f65251865b26d10aa3f900bcdb23
Ernest <32613695+keztheslayer@users.noreply.github.com>
2020-07-15 22:02:24 +0300

    Creating a framework (#3)
    * Create classes
    
    * Components markup dynamic render
    
    * Class for working with DOM
    
    * Preparing event listeners structure
    
    * Added listening to events
    
    * Ability to remove event listeners
    
    * Created table, finished base framework functional
    
    Co-authored-by: Ernest Gilyazov <gilyazov@reg.ru>

---
+++ src/components/table/table.template.js
@@ -0,0 +1,57 @@
+const charA = 65;
+const charZ = 90;
+const baseRowsCount = 4;
+
+function toCell() {
+    return `
+        <div class="cell" contenteditable=""></div>
+    `;
+}
+
+function toColumn( column ) {
+    return `
+        <div class="column">
+            ${column}
+        </div>
+    `;
+}
+
+function createRow( content, index ) {
+    return `
+        <div class="row">
+            <div class="row-info">${index || ''}</div>
+            <div class="row-data">${content}</div>
+        </div>
+    `;
+}
+
+function toChar( _, index ) {
+    return String.fromCharCode( charA + index );
+}
+
+export function createTable( rowsCount = baseRowsCount ) {
+    const colsCount = charZ - charA + 1;
+    const rows = [];
+    const firstRowColumns = new Array( colsCount )
+        .fill()
+        .map( toChar )
+        .map( toColumn )
+        .join('');
+
+    rows.push( createRow( firstRowColumns, null ) );
+
+    for ( let i = 0; i < rowsCount; i++ ) {
+        const cells = new Array( colsCount )
+            .fill()
+            .map( toCell )
+            .join('');
+            
+        rows.push( createRow( cells, i + 1 ) );
+    }
+
+    /* rows.push( createRow(`<div class="cell selected">A1</div>
+    <div class="cell">B2</div>
+    <div class="cell" contenteditable="">C3</div>`) ); */
+
+    return rows.join('');
+}
