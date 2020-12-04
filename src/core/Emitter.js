5a80e7f3c897663a10472532c2d58f8166ddb44e
Ernest Gilyazov <gilyazov@reg.ru>
2020-12-04 20:04:38 +0300

    Creating an observer

---
+++ src/core/Emitter.js
@@ -0,0 +1,28 @@
+export class Emitter {
+    constructor() {
+        this.listeners = {};
+    }
+
+    // notificate listeners
+    emit( event, ...args ) {
+        if ( !Array.isArray( this.listeners[event] ) ) {
+            return false;
+        }
+        this.listeners[event].forEach( listener => {
+            listener( ...args );
+        } );
+
+        return true;
+    }
+
+    // add new listener
+    subscribe( event, fn ) {
+        this.listeners[event] = this.listeners[event] || [];
+        this.listeners[event].push( fn );
+
+        return () => {
+            this.listeners[event] 
+                = this.listeners[event].filter( listener => listener !== fn );
+        };
+    }
+}
