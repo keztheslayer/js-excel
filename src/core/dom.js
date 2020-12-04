6b38b8b28656590938d6b1067cdffc069162434b
Ernest Gilyazov <gilyazov@reg.ru>
2020-12-04 22:15:49 +0300

    Added return for chaining

--- src/core/dom.js
+++ src/core/dom.js
@@ -87,10 +87,14 @@ class Dom {
 
     addClass( className ) {
         this.$el.classList.add( className );
+
+        return this;
     }
 
     removeClass( className ) {
         this.$el.classList.remove( className );
+
+        return this;
     }
 
     id( parse ) {
