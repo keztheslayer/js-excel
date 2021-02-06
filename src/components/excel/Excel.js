1fa5d74e7056b8a5283c2962bf6bd9c2a1a17348
Ernest Gilyazov <gilyazov@reg.ru>
2021-02-06 17:24:26 +0300

    Basic routing

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,8 +3,7 @@ import { Emitter } from '@core/Emitter';
 import { StoreSubscriber } from '@core/storeSubscriber';
 
 export class Excel {
-    constructor( selector, options ) {
-        this.$el = $( selector );
+    constructor( options ) {
         this.components = options.components || [];
         this.store = options.store;
         this.emitter = new Emitter();
@@ -21,25 +20,20 @@ export class Excel {
 
         this.components = this.components.map( Component => {
             const $el = $.create( 'div', Component.className );
-
             const component = new Component( $el, componentOptions );
 
             $el.html( component.toHTML() );
-
             $root.append( $el );
-
+            
             return component;
         } );
 
         return $root;
     }
 
-    render() {
-        this.$el.append( this.getRoot() );
+    init() {
         this.subscriber.subscribeComponents( this.components );
-        this.components.forEach( component => {
-            component.init();
-        } );
+        this.components.forEach( component => component.init() );
     }
 
     destroy() {
